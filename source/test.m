f7bc1d736c743ceecf7113c179f12b407923eca4
ADAM <adamcooman@gmail.com>
2017-08-18 09:57:54 +0200

    updated test function

--- source/test.m
+++ source/test.m
@@ -1,21 +1,17 @@
-clear variables
-close all
-clc
-
-VAR1 = Variable('Name','In1','Type','Double','Description','test param 1');
-VAR2 = Variable('Name','In2','Type','Double','Description',{'test param 2','more info about this guy'});
-VARO = Variable('Name','Out','Type','Double','Description','output parameter');
-
-A = functionHelp('Name','Adam','Tagline','tests the adam function','RequiredInputs',{VAR1,VAR2},'OutputList',{VARO});
-
-A.print
-
 %% 
+
 clear variables
 close all
 clc
 cd C:\Users\Adam\Documents\MATLAB\generateDocumentation\demo\
 code = readTextFile(which('demoFunction.m'));
-res = functionHelp.parse(code);
+res = functionHelp.replaceHelp(code);
 
-res.print
+
+
+%%
+clear variables
+close all
+clc
+cd C:\Users\Adam\Documents\MATLAB\generateDocumentation\demo\
+generateHelp('.')
